08c7e8cea3b90165120553fe4ff0f04e8ea51ed6
Cristovão <cristovao.s.lima@gmail.com>
2022-09-21 21:33:13 -0300

    links header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,25 +6,27 @@ import AddIcon from '@mui/icons-material/Add';
 export default function Header() {
     return (
         <div>
-            <M.AppBar position="fixed">
+            <M.AppBar position="static">
                 <M.Toolbar>
                     <M.Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-                      {/* <Link to=""> */}
+                      <M.Link href="/"color="inherit">
                       Blog
-                      {/* </Link>                       */}
+                      </M.Link>
                     </M.Typography>
-                    <M.IconButton
-                      color="inherit"
-                      aria-label="open drawer"
-                      edge="end"
-                      // onClick={handleDrawerOpen}
-                      // sx={{ ...(open && { display: 'none' }) }}
-                    >
-                      <AddIcon />
-                      <M.Typography noWrap sx={{ flexGrow: 1 }}>
-                      Novo Post
-                      </M.Typography>
-                    </M.IconButton>
+                    <M.Link href="/create">
+                      <M.IconButton
+                        // color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        // onClick={handleDrawerOpen}
+                        // sx={{ ...(open && { display: 'none' }) }}
+                      >
+                        <AddIcon />
+                        <M.Typography noWrap sx={{ flexGrow: 1 }}>
+                        Novo Post
+                        </M.Typography>
+                      </M.IconButton>
+                    </M.Link>                    
                 </M.Toolbar>
             </M.AppBar>
         </div>
